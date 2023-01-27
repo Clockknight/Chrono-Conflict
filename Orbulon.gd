6baae44b23538e80b7c1520afc74fed9eaf90ac1
Clockknight <hello@tylerhanyinwang.com>
2023-01-26 23:55:25 -0800

    Setup char_bottom to reference character's bottommost pixel

--- Orbulon.gd
+++ Orbulon.gd
@@ -1,16 +1,21 @@
 extends KinematicBody2D
 
-export(float) var speed = 100
+export(float) var speed = 10.0
+var directional_input = Vector2.ZERO
+var gravity = .1
 
 
 func _ready():
-	pass # Replace with function body.
+	var _bottom = self.get_node("char_bottom")	
+	
+
+
 
 
 
 func _physics_process(delta):
-	var directional_input = Vector2.ZERO
 	directional_input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	directional_input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	directional_input.y -=  Input.get_action_strength("ui_up")
+	directional_input.y += gravity
 
 	move_and_collide(directional_input * speed)
