91a009f5a3ee16240eb4df939eadab7b2b22952c
Clockknight <hello@tylerhanyinwang.com>
2023-02-05 22:33:47 -0800

    Implement jumping behavior

--- Orbulon.gd
+++ Orbulon.gd
@@ -1,12 +1,15 @@
 extends KinematicBody2D
 
-export(float) var speed = 10.0
+export(float) var horizontal_speed = 10.0
+export(float) var vertical_speed = 50.0
 var directional_input = Vector2.ZERO
 var gravity = .1
-
+var _bottom
+var bottom_pos = 0
+var grounded = true
 
 func _ready():
-	var _bottom = self.get_node("char_bottom")	
+	_bottom = self.get_node("char_bottom")
 	
 
 
@@ -15,7 +18,16 @@ func _ready():
 
 func _physics_process(delta):
 	directional_input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	directional_input.y -=  Input.get_action_strength("ui_up")
+	directional_input.x *= horizontal_speed
+	
 	directional_input.y += gravity
+	bottom_pos = (_bottom.get_global_position().y)
+	grounded = bottom_pos >= 0
+	if (grounded):
+		directional_input.y = 0
+		directional_input.y -=  Input.get_action_strength("ui_up")
+	directional_input.y *= vertical_speed
+	
+	
 
-	move_and_collide(directional_input * speed)
+	move_and_collide(directional_input)
