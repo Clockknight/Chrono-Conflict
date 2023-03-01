e50555dce0612a991772883608fb0573a9a73d64
Clockknight <hello@tylerhanyinwang.com>
2023-02-28 23:43:35 -0800

    manager skeleton

---
+++ manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	# Find player 2 objects
+	# run config() on each, setting their control strings as needed depending on which is p1/p2
+	pass
+
+
+#func for accepting win/loss
