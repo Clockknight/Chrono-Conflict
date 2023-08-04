0e83dd8b8c58ae3f754abb504dac2358e31da40c
Clockknight <34818025+Clockknight@users.noreply.github.com>
2023-08-03 22:08:22 -0400

    skeleton for hitbox interactions

--- Scripts/Boxes/Hit_Box.gd
+++ Scripts/Boxes/Hit_Box.gd
@@ -1,16 +1,25 @@
 class_name Hit_Box
 extends Box
 
+var test = []
+
+func _ready():
+	connect("area_entered", self, "_on_area_entered")
+
+func _on_area_entered(object) -> void:
+	test.append(object)
+	print("!!!!")
+
 func tick():
-	#check for hurt boxes
-	#if seen
-	#report to hurt
-	#report to parent
-	
-	#otherwise
-	# if hitbox seen
-	#report to hurt
-	#report to parent	
+	#check for hurt boxes	
+		#if this box is overlapping a hurtbox
+		#report to hurtbox
+		#report to own parent
+		
+		#otherwise
+		# if hitbox seen
+		#report to hitbox
+		#report to own parent
 	.tick()
 	
 	
