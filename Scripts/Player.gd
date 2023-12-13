df69ff44867332667e4fbb45e77655dbd3bb495d
Clockknight <hello@tylerhanyinwang.com>
2023-12-12 23:22:09 -0500

    Put input interpretation in its own function to be overriden per character
    Might be able to set this up differently in the future, but it should be okay for now.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -222,15 +222,7 @@ func _read_input():
 	return _cur_input
 
 func _interpret_inputs(values:Input_Data):
-	if _state == e.State.FREE or (_state_frames_left <= BUFFER_WINDOW and _state_queue == []):
-		if Input.is_action_pressed(_up_string):
-			_parse_states(['JMPS|5'])
-			_cur_x = _stored_x
-			_stored_x = _cur_input['x']
-		if Input.is_action_just_pressed(_a_string):
-			spawn_box()
-			_parse_states(['ACTV|15'])
-			
+	_debug_message("_Interpret_inputs Not Inherited!")
 
 		
 func _state_tick():
