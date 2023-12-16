568eb101c180421374ef76a9174fb99380887dfe
Clockknight <hello@tylerhanyinwang.com>
2023-12-15 23:28:19 -0500

    Update inputs to read from input

--- Scripts/Orbulon.gd
+++ Scripts/Orbulon.gd
@@ -10,25 +10,30 @@ func _ready():
 
 
 
-func _interpret_inputs(values:Input_Data):
+func _interpret_inputs(input:Input_Data):
 	if _state == e.State.FREE or (_state_frames_left <= BUFFER_WINDOW and _state_queue == []):
 		
 		var frame_data = null
 		
+		_debug_message(str(input.report(false, 1)))
 		
 		# Movement block (lowest priority)		
-		if Input.is_action_pressed(_up_string):
+		if input.y < 0:
 			frame_data = ['JMPS|5']
 			_cur_x = _stored_x
 			_stored_x = _cur_input['x']
 			
 		# Normal Block
-		if Input.is_action_just_pressed(_a_string):
+		if input.a == true:
 			spawn_box()
 			frame_data = ['ACTV|15','RECV|5']
 
 		#Command Normal Block
-		# if newest input data has forward and also a, use a different move
+		if input.a == true and input.x *  int(_p1_side) > 0:
+			spawn_box()
+			spawn_box()
+			frame_data = ['ACTV|15','RECV|5']
+		
 		
 		# Special Block
 		
