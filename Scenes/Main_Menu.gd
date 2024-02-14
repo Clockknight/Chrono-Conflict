0db95dc0043622ea3db9a530791d0b0437f4f18e
Clockknight <hello@tylerhanyinwang.com>
2024-02-13 23:47:28 -0500

    Add script skeleton for main menu

---
+++ Scenes/Main_Menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+# Should check for new inputs from user like checking for attacks
+# On up, down or a1 from either user, change boxes appropriately
+#
