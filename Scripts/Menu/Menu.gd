71977bb71d6fb1e9d654822abec0f8bca3f7a6a0
Clockknight <hello@tylerhanyinwang.com>
2024-02-25 22:50:38 -0500

    Add init function for menu objects

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -1,11 +1,18 @@
 extends Control
 
 var preload_Button = load("res://Scenes/Menu/button.tscn")
-
+var buttons 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+#func _ready():
+	
 	
 
+func menu_init(length:int):
+	buttons = []
+	
+	for x in range(0,length):
+		buttons[x] = preload_Button.instantiate()
+		buttons[x].button_set()
 
 # Should check for new inputs from user like checking for attacks
 # On up, down or a1 from either user, change boxes appropriately
