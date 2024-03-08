a92552645bbe5ddccce06e6bd5410c31d0fbbf81
Clockknight <hello@tylerhanyinwang.com>
2024-03-07 20:46:49 -0500

    Update Menu.gd

--- Scripts/Menu/Menu.gd
+++ Scripts/Menu/Menu.gd
@@ -1,23 +1,26 @@
 extends Control
 
 var preload_Button = load("res://Scenes/Menu/button.tscn")
+var menu_location = "res://Data/menus.cfg"
 var active_index = 0
 
 var content
 var label
 var buttons
 var array_2d
+var length
+var index
 
 #func _ready():
 
 func menu_init(menu_id:String):
-		
-	if FileAccess.file_exists(dict_location):
-		var file = FileAccess.get_file_as_string(dict_location)	
-		res = JSON.parse_string(file)[menu_id]
+	if FileAccess.file_exists(menu_location):
+		var file = FileAccess.get_file_as_string(menu_location)	
+		var res = JSON.parse_string(file)[menu_id]
 		if res != null:
 			content = res
 
+var data = {"sam_id":{"name":"asdasd", "buttons":["id1","id2","id3"], "2d":false}}
 
 
 func cycle(downwards:bool):
@@ -26,17 +29,16 @@ func cycle(downwards:bool):
 	else:
 		active_index -= 1
 		
-	if downwards < 0:
+	if index < 0:
 		downwards = buttons.size()
 	
 	
-var data = {"sam_id":{"name":"asdasd", "buttons":["id1","id2","id3"], "2d":false}}
 
  
 func left(stack):
 	
 	
-	
+	'''
 func right(stack):
 	
 func accept(stack):
@@ -47,3 +49,4 @@ func back(stack):
 	
 
 	
+'''
