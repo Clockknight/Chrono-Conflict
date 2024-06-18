7dd70776593dce67d31e09f89125a12b473306d0
Clockknight <hello@tylerhanyinwang.com>
2024-06-18 00:14:36 -0400

    Update Enums.gd

--- Data/Enums.gd
+++ Data/Enums.gd
@@ -1,71 +1,40 @@
 extends Node
 
-
 enum State {
-	FREE=0, # Non-active
-	STRT=1, # Startup 
-	ACTV=2, # Active
-	RECV=3, # Recovery
-	STUN=4, # Stun/ Histun
-	BLCK=5, # Blockstun
-	JMPB=6, # Jump Begin
-	JMPF=7, # Jump FREE
-	JMPS=8, # Jump STRT
-	JMPA=9, # Jump ACTV
-	JMPR=10 # Jump RECV
-	}
- 
-
-
+	FREE = 0,  # Non-active
+	STRT = 1,  # Startup
+	ACTV = 2,  # Active
+	RECV = 3,  # Recovery
+	STUN = 4,  # Stun/ Histun
+	BLCK = 5,  # Blockstun
+	JMPB = 6,  # Jump Begin
+	JMPF = 7,  # Jump FREE
+	JMPS = 8,  # Jump STRT
+	JMPA = 9,  # Jump ACTV
+	JMPR = 10  # Jump RECV
+}
 
 enum Level {
 	# For reports that occur once per sub-tick (eg movement_tick's calculations)
-	CALC, 
+	CALC,
 	# For reports that occur once per frame (eg Tick and Sub Tick)
-	FRAME, 
+	FRAME,
 	# For reports that occur ocassionally (eg player dying/taking damage)
-	EVENT, 
+	EVENT,
 	# For reports on something that shouldnt happen
-	ERROR, 
+	ERROR,
 	# For unspecified events, for debugging
-	DEBUG}
-	
-enum Type{
-	LOW=0,
-	MID=1,
-	HIG=2,
-	GRB=3,
-	UNB=4
+	DEBUG
 }
 
-enum Hit{
-	BLCK=0,
-	HURT=1,
-	CNTR=2,
-	GRAB=3
-}
+enum Type { LOW = 0, MID = 1, HIG = 2, GRB = 3, UNB = 4 }
 
-enum Elem{
-	HEALTH = 0,
-	CONSOLE = 1
-	
-	
-}
+enum Hit { BLCK = 0, HURT = 1, CNTR = 2, GRAB = 3 }
 
-enum Load{
-	SCENE=0,
-	BUNDLE=1,
-	ASSET=2
-}
+enum Elem { HEALTH = 0, CONSOLE = 1 }
 
-enum AudioTypes{
-	MASTER=0,
-	SFX=1,
-	DIA=2,
-	MUS=3,
-	CAL=4
-}
+enum Load { SCENE = 0, BUNDLE = 1, ASSET = 2 }
 
-enum Constants{
-	BUFFER_WINDOW = 3
-}
+enum AudioTypes { MASTER = 0, SFX = 1, DIA = 2, MUS = 3, CAL = 4 }
+
+enum Constants { BUFFER_WINDOW = 3, SIMULTANEOUS_WINDOW = 2, HISTORY_WINDOW = 30 }
